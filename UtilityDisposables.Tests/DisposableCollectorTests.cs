3c55291271e867f6cad1ed88e0eb604c064a298e
nloum <39776302+nloum@users.noreply.github.com>
2020-02-02 19:23:52 -0600

    Start adding tests

---
+++ UtilityDisposables.Tests/DisposableCollectorTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UtilityDisposables.Tests
+{
+    [TestClass]
+    public class DisposableCollectorTests
+    {
+        [TestMethod]
+        public void ShouldDisposeMultipleDisposables()
+        {
+            var disposable1 = new TestDisposable();
+            var disposable2 = new TestDisposable();
+            var uut = new DisposableCollector(disposable1);
+            uut.Disposes(disposable1);
+            uut.Dispose();
+            disposable1.IsDisposed.Should().BeTrue();
+            disposable2.IsDisposed.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void ShouldFailIfTheSameDisposableIsAddedMultipleTimes()
+        {
+            var disposable1 = new TestDisposable();
+            var uut = new DisposableCollector(disposable1);
+            Action action = () => uut.Disposes(disposable1);
+            action.Should().Throw<Exception>();
+        }
+    }
+}
---
+++ UtilityDisposables.Tests/TestDisposable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace UtilityDisposables.Tests
+{
+    public class TestDisposable : IDisposable
+    {
+        public bool IsDisposed { get; private set; } = false;
+
+        public void Dispose()
+        {
+            IsDisposed = true;
+        }
+    }
+}
