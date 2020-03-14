ca54a43fff7509c48560720cecb64cfab6fd46d8
nloum <39776302+nloum@users.noreply.github.com>
2020-03-14 12:39:59 -0500

    Only run builds on push, not pull request #1

--- build/Build.cs
+++ build/Build.cs
@@ -21,7 +21,7 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 	GitHubActionsImage.Ubuntu1804,
 	ImportSecrets = new[]{ "NUGET_API_KEY" },
 	AutoGenerate = true,
-	On = new [] { GitHubActionsTrigger.Push, GitHubActionsTrigger.PullRequest },
+	On = new [] { GitHubActionsTrigger.Push },
 	InvokedTargets = new [] {"Test", "Push"}
 	)]
 class Build : NukeBuild
